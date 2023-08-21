6a12c35f22044b4dd27ab85f7929727d39942b9f
vagrant <hassan.a.issak@gmail.com>
2023-08-21 19:35:56 +0000

    Continue to define Game class

--- Game.rb
+++ Game.rb
@@ -16,6 +16,15 @@ include Question
       puts '-----NEW TURN-----'
       question.generate_question
       player_answer = gets.chomp
+      if (question.check_answer(player_answer))
+        puts "YES! You are correct, #{@current_player}"
+      elsif (!question.check_answer(player_answer))
+        puts "Seriously? Go back to school, #{@current_player}"
+        @current_player.lose_life
+      end
+      puts @player1.to_s + " " + @player2.to_s
+      
+
 
 
 
