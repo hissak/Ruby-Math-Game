a5a545758e12321bad98a9e406a238e3ce323fac
vagrant <hassan.a.issak@gmail.com>
2023-08-21 19:28:38 +0000

    Partially constructs Game clase

--- Game.rb
+++ Game.rb
@@ -1,9 +1,22 @@
 import './Player.rb'
 import './Question.rb'
 Class Game
-attr_accessor :player1 :player2 :current_player :game_over
-def initialize
-  @player1 = 
+include Player
+include Question
+  attr_accessor :player1, :player2, :current_player, :game_over
+  def initialize
+    @player1 = Player.new('Numero Uno', 3)
+    @player2 = Player.new('Numero Dos', 3)
+    @current_player = @player1
+    @game_over = false
+  end
+  def start_game
+    while !game_over
+      question = Question.new
+      puts '-----NEW TURN-----'
+      question.generate_question
+      player_answer = gets.chomp
+
 
 
 
--- Question.rb
+++ Question.rb
@@ -1,8 +1,8 @@
 Class Question
   attr_accessor :num1, :num2, :answer
   def initialize
-    @num1 = rand(1..20)
-    @num2 = rand(1..20)
+    @num1 = rand(1..10)
+    @num2 = rand(1..10)
     @answer = num1 + num2
   end
   def generate_question
