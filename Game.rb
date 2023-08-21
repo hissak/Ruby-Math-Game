1909b150d20eee90d745c93315c2cb71c9b213a3
vagrant <hassan.a.issak@gmail.com>
2023-08-21 21:04:07 +0000

    Fix typos

--- Game.rb
+++ Game.rb
@@ -2,12 +2,14 @@ require './Player.rb'
 require './Question.rb'
 class Game
   attr_accessor :player1, :player2, :current_player, :game_over, :winner, :lives
+
   def initialize
     @player1 = Player.new('Numero Uno', 3)
     @player2 = Player.new('Numero Dos', 3)
     @current_player = @player1
     @game_over = false
   end
+
   def start_game
     puts '-----NEW GAME-----'
     while !game_over
@@ -17,9 +19,9 @@ class Game
       puts "Player #{@current_player}:"
       player_answer = gets.chomp
       if (question.check_answer?(player_answer))
-        puts "YES! You are correct, #{@current_player}"
+        puts "YES! You are correct, #{@current_player}!"
       elsif (!question.check_answer?(player_answer))
-        puts "Seriously? Go back to school, #{@current_player}"
+        puts "Seriously? Go back to school, #{@current_player}!"
         @current_player.lose_life
       end
       puts @player1.player + ", " + @player2.player
