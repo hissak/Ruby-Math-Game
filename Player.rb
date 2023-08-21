6dc0e232c5e5f83b1636a5d59f5eaa871553caf7
vagrant <hassan.a.issak@gmail.com>
2023-08-21 17:02:57 +0000

    Define Player class

---
+++ Player.rb
@@ -0,0 +1,16 @@
+Class Player {
+  attr_accessor :name, :lives
+  def initialize(name, lives)
+    @name = name
+    @lives = lives
+  end
+  def lose_life
+    lives - 1
+  end
+  def is_dead?
+    lives == 0
+  end
+  def to_s
+    "#{name}: #{lives}/3"
+  end
+}
